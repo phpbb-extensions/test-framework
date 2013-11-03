e56803e2dc4a27590321bd065b30e31b277483b4
Nathan Guse <nathaniel.guse@gmail.com>
2013-11-03 16:06:52 -0600

    Moving everything to prepare-travis.sh so an extension author can just add one .travis.yml file to their ext and everything will be handled.

---
+++ prepare-travis.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# @copyright (c) 2013 phpBB Group
+# @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+#
+
+# clone phpBB
+git clone "git://github.com/phpbb/phpbb.git" "./../../phpbb"
+
+# make the extension directory
+mkdir -p ./../../phpbb/phpBB/ext/$VENDOR/$NAME/travis
+
+# move the current working directory (which contains the root of the main repository) to the extension directory
+mv * ./../../phpbb/phpBB/ext/$VENDOR/$NAME
+
+# move the test-framework/travis files to the ext dir
+mv ./../../test-framework/travis/* ./../../phpbb/phpBB/ext/$VENDOR/$NAME/travis
+
+# move ourselves to the root phpBB directory
+cd ./../../phpbb/phpBB
+
+# Setup phpBB dependencies
+php ./../composer.phar install --dev --no-interaction --prefer-source
+
+# move ourselves to the root directory of the checked out phpbb repository
+cd ./../
+
+# Setup the tests/travis
+sh -c "if [ '$DB' = 'postgres' ]; then psql -c 'DROP DATABASE IF EXISTS phpbb_tests;' -U postgres; fi"
+sh -c "if [ '$DB' = 'postgres' ]; then psql -c 'create database phpbb_tests;' -U postgres; fi"
+sh -c "if [ '$DB' = 'mariadb' ]; then ./travis/setup-mariadb.sh; fi"
+sh -c "if [ '$DB' = 'mysql' ]; then mysql -e 'create database IF NOT EXISTS phpbb_tests;'; fi"
+./travis/install-php-extensions.sh
+phpenv rehash
+sh -c "if [ `php -r "echo (int) version_compare(PHP_VERSION, '5.3.19', '>=');"` = "1" ]; then ./travis/setup-webserver.sh; fi"
