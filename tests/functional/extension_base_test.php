4c7c6446e24ab8e7bb27c0a1defb0334ff109f45
Matt Friedman <maf675@gmail.com>
2014-02-26 15:12:54 -0800

    Add a test of the extension test case

---
+++ tests/functional/extension_base_test.php
@@ -0,0 +1,49 @@
+<?php
+/**
+*
+* @package testing
+* @copyright (c) 2014 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* @group functional
+*/
+class extension_functional_extension_base_test extends extension_functional_test_case
+{
+	public function setUp()
+	{
+		parent::setUp();
+
+		$this->vendor = 'phpbb';
+		$this->extension = 'example';
+		$this->display_name = 'phpBB Example Extension'
+		
+		$this->login();
+		$this->admin_login();
+		$this->set_extension($this->vendor, $this->extension, $this->display_name);
+	}
+
+	/**
+	* Test enabling an extension
+	*
+	* @access public
+	*/
+	public function test_enable_extension()
+	{
+		$this->enable_extension();
+
+		$crawler = self::request('GET', 'adm/index.php?i=acp_extensions&mode=main&sid=' . $this->sid);
+		$enabled_extensions = $crawler->filter('tr.ext_enabled')->extract(array('_text'));
+		foreach ($enabled_extensions as $extension)
+		{
+			if (strpos($extension, $this->display_name) !== 0)
+			{
+				continue;
+			}
+			
+			$this->assertEquals($this->display_name, $extension);
+		}
+	}
+}
