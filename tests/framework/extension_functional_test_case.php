4aef182143350ef7026ba141dc9dc57c8d635696
Matt Friedman <maf675@gmail.com>
2014-02-27 12:51:49 -0800

    Fix variable names and pass by ref

--- tests/framework/extension_functional_test_case.php
+++ tests/framework/extension_functional_test_case.php
@@ -156,7 +156,7 @@ abstract class extension_functional_test_case extends phpbb_functional_test_case
 
 		$is_disabled = false;
 
-		$crawler->filter('tr.ext_disabled')->each(function ($node, $i) use ($is_disabled) {
+		$crawler->filter('tr.ext_disabled')->each(function ($node, $i) use (&$is_disabled) {
 			$children = $node->children();
 			if (strpos($children[0]->text(), $this->extension_display_name) !== false && strpos($children[2]->text(), $this->lang('EXTENSION_DELETE_DATA')) !== false)
 			{
@@ -179,7 +179,7 @@ abstract class extension_functional_test_case extends phpbb_functional_test_case
 
 		$is_available = false;
 
-		$crawler->filter('tr.ext_disabled')->each(function ($node, $i) use ($is_disabled) {
+		$crawler->filter('tr.ext_disabled')->each(function ($node, $i) use (&$is_available) {
 			$children = $node->children();
 			if (strpos($children[0]->text(), $this->extension_display_name) !== false && strpos($children[2]->text(), $this->lang('EXTENSION_DELETE_DATA')) === false)
 			{
