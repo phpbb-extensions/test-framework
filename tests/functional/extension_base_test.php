9dae011f96768106d591071f1e7aec640076ad09
Matt Friedman <maf675@gmail.com>
2014-02-26 18:27:00 -0800

    Merge all functions into one for testing

--- tests/functional/extension_base_test.php
+++ tests/functional/extension_base_test.php
@@ -26,14 +26,16 @@ class extension_functional_extension_base_test extends extension_functional_test
 	}
 
 	/**
-	* Test enabling an extension
+	* Test enabling / disabling / purging of an extension
 	*
 	* @access public
 	*/
-	public function test_enable_extension()
+	public function test_enable__diabel_purge_extension()
 	{
+		// Enable extension
 		$this->enable_extension();
 
+		// Test enabled extension exists
 		$crawler = self::request('GET', 'adm/index.php?i=acp_extensions&mode=main&sid=' . $this->sid);
 		$enabled_extensions = $crawler->filter('tr.ext_enabled')->extract(array('_text'));
 		foreach ($enabled_extensions as $extension)
@@ -45,17 +47,11 @@ class extension_functional_extension_base_test extends extension_functional_test
 			
 			$this->assertEquals($this->display_name, $extension);
 		}
-	}
 
-	/**
-	* Test disabling an extension
-	*
-	* @access public
-	*/
-	public function test_disable_extension()
-	{
+		// Disable extension
 		$this->disable_extension();
 
+		// Test disabled extension exists
 		$crawler = self::request('GET', 'adm/index.php?i=acp_extensions&mode=main&sid=' . $this->sid);
 		$disabled_extensions = $crawler->filter('tr.ext_disabled')->extract(array('_text'));
 		foreach ($disabled_extensions as $extension)
@@ -67,17 +63,11 @@ class extension_functional_extension_base_test extends extension_functional_test
 			
 			$this->assertEquals($this->display_name, $extension);
 		}
-	}
 
-	/**
-	* Test purging an extension
-	*
-	* @access public
-	*/
-	public function test_purge_extension()
-	{
+		// Purge extension
 		$this->purge_extension();
 
+		// Test purged extension exists
 		$crawler = self::request('GET', 'adm/index.php?i=acp_extensions&mode=main&sid=' . $this->sid);
 		$disabled_extensions = $crawler->filter('tr.ext_disabled')->extract(array('_text'));
 		foreach ($disabled_extensions as $extension)
