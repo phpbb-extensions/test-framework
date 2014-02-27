fe026debdce27e66f83331834261f37abf68284b
Matt Friedman <maf675@gmail.com>
2014-02-26 16:28:44 -0800

    Add travis stuff with updated bootstrap

---
+++ travis/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+/**
+*
+* @package testing
+* @copyright (c) 2014 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+// Require the phpBB test bootstrap file
+require dirname(__FILE__) . '/../../../../../tests/bootstrap.php';
+
+// Require our base extension database class
+require_once dirname(__FILE__) . '/../tests/framework/extension_database_test_case.php';
+
+// Require our base extension functional class
+require_once dirname(__FILE__) . '/../tests/framework/extension_functional_test_case.php';
