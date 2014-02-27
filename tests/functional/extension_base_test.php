4a8e94186570cd9182b5bc7de73b0a2578d52997
Matt Friedman <maf675@gmail.com>
2014-02-26 18:48:05 -0800

    Use assertTrue to check if functional tests worked

--- tests/functional/extension_base_test.php
+++ tests/functional/extension_base_test.php
@@ -34,50 +34,14 @@ class extension_functional_extension_base_test extends extension_functional_test
 	{
 		// Enable extension
 		$this->enable_extension();
-
-		// Test enabled extension exists
-		$crawler = self::request('GET', 'adm/index.php?i=acp_extensions&mode=main&sid=' . $this->sid);
-		$enabled_extensions = $crawler->filter('tr.ext_enabled')->extract(array('_text'));
-		foreach ($enabled_extensions as $extension)
-		{
-			if (strpos($extension, $this->display_name) !== 0)
-			{
-				continue;
-			}
-			
-			$this->assertEquals($this->display_name, $extension);
-		}
+		$this->assertTrue(is_enabled());
 
 		// Disable extension
 		$this->disable_extension();
-
-		// Test disabled extension exists
-		$crawler = self::request('GET', 'adm/index.php?i=acp_extensions&mode=main&sid=' . $this->sid);
-		$disabled_extensions = $crawler->filter('tr.ext_disabled')->extract(array('_text'));
-		foreach ($disabled_extensions as $extension)
-		{
-			if (strpos($extension, $this->display_name) !== 0)
-			{
-				continue;
-			}
-			
-			$this->assertEquals($this->display_name, $extension);
-		}
+		$this->assertTrue(is_disabled());
 
 		// Purge extension
 		$this->purge_extension();
-
-		// Test purged extension exists
-		$crawler = self::request('GET', 'adm/index.php?i=acp_extensions&mode=main&sid=' . $this->sid);
-		$disabled_extensions = $crawler->filter('tr.ext_disabled')->extract(array('_text'));
-		foreach ($disabled_extensions as $extension)
-		{
-			if (strpos($extension, $this->display_name) !== 0)
-			{
-				continue;
-			}
-			
-			$this->assertEquals($this->display_name, $extension);
-		}
+		$this->assertTrue(is_available());
 	}
 }
