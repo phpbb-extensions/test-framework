82af1f4ba60c0d5a2381b804037670636023b241
Matt Friedman <maf675@gmail.com>
2014-02-27 17:57:39 -0800

    Add extension enable/disable/purge functional test

---
+++ ext.php
@@ -0,0 +1,14 @@
+<?php
+/**
+*
+* @package testing
+* @copyright (c) 2014 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace phpbb\example;
+
+class ext extends \phpbb\extension\base
+{
+}
